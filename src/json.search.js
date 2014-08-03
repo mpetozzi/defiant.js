8b9d15a25220284bf6f395054ed1ad8bbc45eabc
Hakan Bilgin <hbi99@hotmail.com>
2014-08-03 20:03:53 +0200

    tests

--- src/json.search.js
+++ src/json.search.js
@@ -3,32 +3,36 @@ if (!JSON.search) {
 	JSON.search = function(tree, xpath, single) {
 		'use strict';
 		
-		var doc  = JSON.toXML(tree),
-			xres = Defiant.node[ single ? 'selectSingleNode' : 'selectNodes' ](doc, xpath),
-			i    = xres.length,
-			ret  = [],
-			mapIndex;
+		var xTree     = JSON.toXML(tree),
+			xFound    = Defiant.node[ single ? 'selectSingleNode' : 'selectNodes' ](xTree, xpath),
+			i         = xFound.length,
+			found     = [],
+			searchMap = this.search.map,
+			mapIndex,
+			xmlTwin;
 
-		if (single) xres = [xres];
+		if (single) xFound = [xFound];
 
-		//console.log( 'x-RES:', xres );
+		//console.log( 'x-RES:', xFound );
 		while (i--) {
-			switch(xres[i].nodeType) {
+			xmlTwin = xFound[i];
+			switch(xmlTwin.nodeType) {
 				case 2:
 				case 3: 
-					ret.unshift( xres[i].nodeValue );
+					found.unshift( xmlTwin.nodeValue );
 					break;
 				default:
-					mapIndex = +xres[i].getAttribute('d:mi');
-					ret.unshift( this.search.map[mapIndex-1] );
+					mapIndex = +xmlTwin.getAttribute('d:mi');
+					found.unshift( searchMap[mapIndex-1] );
 			}
+			found[0].twin = xmlTwin;
 		}
 		// if environment = development, add search tracing
 		if (Defiant.env === 'development') {
-			this.trace = JSON.mtrace(tree, ret, xres);
+			this.trace = JSON.mtrace(tree, found, xFound);
 		}
 
-		//console.log( 'RES:', ret );
-		return ret;
+		//console.log( 'RES:', found );
+		return found;
 	};
 }
